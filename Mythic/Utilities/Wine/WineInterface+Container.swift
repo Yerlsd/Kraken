9d808ead2b4d9df1f305b7b7a5d7432631eea073
Yerlsd <304683975+Yerlsd@users.noreply.github.com>
2026-09-20 00:32:09 +0100

    Revert experimental change after CI failure

--- Mythic/Utilities/Wine/WineInterface+Container.swift
+++ Mythic/Utilities/Wine/WineInterface+Container.swift
@@ -133,11 +133,11 @@ extension Wine.Container {
 
         init(metalHUD: Bool = false,
              msync: Bool = true,
-             retinaMode: Bool = false,
+             retinaMode: Bool = true,
              dxvk: Bool = false,
              dxvkAsync: Bool = false,
              windowsVersion: Wine.WindowsVersion = .win11,
-             scaling: Int = 96,
+             scaling: Int = 192,
              avx2: Bool = true) {
             self.metalHUD = metalHUD
             self.msync = msync
